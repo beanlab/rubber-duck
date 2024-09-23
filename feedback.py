5260bf5b4e9ac72f565971966bae89b00359c7c8
Bryce Martin <brycemartin52@gmail.com>
2024-09-23 10:14:31 -0600

    Add saturday's work, reporting and ta_feedback

--- feedback.py
+++ feedback.py
@@ -24,7 +24,7 @@ class FeedbackWorkflow:
         Takes thread_id, sends it to the ta-channel, collect's feedback
         """
         async with queue("feedback", None) as feedback_queue:
-            message_content = f"<@{user_id}>, on a scale of 1 to 5, how helpful was this conversation? (Add your reaction below)"
+            message_content = f"<@{user_id}>, on a scale of 1 to 5, how helpful was this conversation https://discord.com/channels/{guild_id}/{thread_id}user_id%7D? (Add your reaction below)"
 
 
             feedback_message = await self._send_message(self._feedback_channel_id, message_content)
@@ -33,7 +33,7 @@ class FeedbackWorkflow:
             #
 
             try:
-                feedback_emoji = await asyncio.wait_for(feedback_queue.get(), timeout=60 * 60 * 24)
+                feedback_emoji = await asyncio.wait_for(feedback_queue.get(), timeout=60 * 60 * 24 * 7)
                 feedback_score = self._reactions[feedback_emoji]
                 await self._send_message(thread_id, f'Thank you for your feedback!')
 
--- report.py
+++ report.py
@@ -4,15 +4,43 @@ import matplotlib.pyplot as plt
 import seaborn as sbn
 import json
 
-# class ReportAssistant()
+
+class ReportAssistant():
+    def __init__(self):
+        self.usage = pd.read_csv("./state/metrics/usage.csv")
+        # self.messages = pd.read_csv("./state/metrics/messages.csv")
+        self.feedback = pd.read_csv("./state/metrics/feedback.csv")
+
+    def read_df(self, df: pd.DataFrame, n=10):
+        print(df.head(n))
+        return df
+
+    def view_graph(self, df: pd.DataFrame, type = 'bar chart'):
+        pass
 
 
-def main():
-    print("Howdy rubber duck")
-    usage = pd.read_csv("./state/metrics/usage.csv")
-    messages = pd.read_csv("./state/metrics/messages.csv")
-    feedback = pd.read_csv("./state/metrics/feedback.csv")
-    print(usage)
+def get_df(assistant: ReportAssistant):
+    while True:
+        df_request = input("Which data do you want to view from? Usage, messages, or feedback?\n").lower()
+        match df_request:
+            case 'usage' | 'u':
+                return assistant.usage
+            case 'feedback' | 'f':
+                return assistant.feedback
+            case 'q':
+                print("See you next time!")
+                return None
+            case _:
+                print(f"Sorry, '{df_request}' is not an option.")
+
+
+
+def main(assistant: ReportAssistant):
+    print("Seems like you'd like to do some data analytics!")
+    df = get_df(assistant)
+    assistant.read_df(df)
+
+    print("Hope that was helpful!")
 
 if __name__ == "__main__":
-    main()
+    main(ReportAssistant())
