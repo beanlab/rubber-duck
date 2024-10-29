758d67be7259b6060ab78d7cf0821e0a66091418
Wiley Welch <wileyjwelch1@gmail.com>
2024-10-29 17:44:32 -0600

    Fixed the docker assembly! So everything seems to be correct except the permissions on the ECR

---
+++ build_docker.sh
@@ -0,0 +1,34 @@
+# Build Rubber Duck Docker
+# this is the command to build the docker image
+# ./build_docker.sh
+IMAGE_NAME="rubber-duck"
+IMAGE_TAG="latest"
+
+docker build \
+    -t ${IMAGE_NAME}:${IMAGE_TAG} \
+    -f - . <<EOF
+
+
+FROM python:3.11.9
+
+
+LABEL authors="Wiley Welch, Bryce Martin, Gordon Bean"
+
+COPY rubber_duck /rubber_duck
+ADD pyproject.toml /rubber_duck/pyproject.toml
+
+ENV OPENAI_API_KEY=${OPENAI_API_KEY}
+
+WORKDIR /rubber_duck
+
+RUN pip install poetry
+
+RUN poetry config virtualenvs.create false \
+    && poetry install --no-interaction --no-ansi
+
+CMD ["python", "/rubber_duck/discord_bot.py", "--config", "/config.json", "--log-console"]
+
+
+EOF
+
+docker pull ${IMAGE_NAME}:${IMAGE_TAG}
