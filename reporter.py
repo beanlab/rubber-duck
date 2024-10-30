06218da5164a12f1342ec459331dbc483b3760af
Bryce Martin <brycemartin52@gmail.com>
2024-10-30 10:57:46 -0600

    Prettify code

--- reporter.py
+++ reporter.py
@@ -8,16 +8,16 @@ from argparse import ArgumentParser, ArgumentError
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
-class csvHandler():
-    def __init__(self):
-        self.path = "state/metrics/"
-        self.df_options = {'feedback', 'usage', 'messages'}
-
-    def select_dataframe(self, desired_df):
-        """Load the dataframe based on user input."""
-        if desired_df not in self.df_options:
-            raise ArgumentError(None, f"Invalid dataframe: {desired_df}")
-        return pd.read_csv(f"{self.path}{desired_df}.csv")
+# class csvHandler():
+#     def __init__(self):
+#         self.path = "state/metrics/"
+#         self.df_options = {'feedback', 'usage', 'messages'}
+#
+#     def select_dataframe(self, desired_df):
+#         """Load the dataframe based on user input."""
+#         if desired_df not in self.df_options:
+#             raise ArgumentError(None, f"Invalid dataframe: {desired_df}")
+#         return pd.read_csv(f"{self.path}{desired_df}.csv")
 
 class Reporter():
     image_cache = {}
@@ -53,11 +53,11 @@ class Reporter():
         'u2': '!report -df usage -iv output_tokens -p year -ev guild_id -ln'
     }
 
-    csvHandler = csvHandler()
 
     def __init__(self, metricsHandler, show_fig=False):
         self.metricsHandler = metricsHandler
         self.show_fig = show_fig
+
 
     def select_dataframe(self, desired_df):
         if desired_df == 'feedback':
@@ -77,16 +77,36 @@ class Reporter():
         return row['input_tokens'] / 1000 * ip + row['output_tokens'] / 1000 * op
 
 
+    ## Not sure the best way to do this, it could turn into a long function of a bunch of if statements if it continues like this
     def preprocessing(self, df, args):
         if args.dataframe == 'usage':
             df['cost'] = df.apply(self.compute_cost, axis=1)
+
+        if args.exp_var == 'guild_id':
+            df['guild_name'] = df['guild_id'].map(self.guilds)
+            df = df.drop(columns=['guild_id'])#.rename(columns={'guild_name': 'guild_name'})
+            args.exp_var = 'guild_name'
 
         df = df.dropna(subset=[args.ind_var])
         return df
 
 
-    def edit_df(self, df, args):
-        """Filter and pivot the dataframe based on user inputs."""
+    def catch_known_issues(self, df, args):
+        if args.ind_var not in df.columns:
+            raise ArgumentError(None, f"Invalid independent variable: {args.ind_var}")
+
+        if args.exp_var and args.exp_var not in df.columns:
+            raise ArgumentError(None, f"Invalid explanatory variable: {args.exp_var}")
+
+        if args.period and args.period not in self.time_periods:
+            raise ArgumentError(None, f"Invalid time period: {args.period}")
+
+
+    def prepare_df(self, df, args):
+        df = self.preprocessing(df, args)
+
+        self.catch_known_issues(df, args)
+
         if args.period:
             df['timestamp'] = pd.to_datetime(df['timestamp'])
             now = datetime.now(ZoneInfo('US/Mountain'))
@@ -108,36 +128,34 @@ class Reporter():
         else:
             df_grouped = df[args.ind_var].reset_index()
 
-        df_pretty = self.prettify_df(df_grouped, args)
-
-        return df_pretty
-
-    ## Not sure the best way to do this, it could turn into a long function of a bunch of if statements if it continues like this
-    def prettify_df(self, df, args):
-        if args.exp_var == 'guild_id':
-            df['guild_name'] = df['guild_id'].map(self.guilds)
-            df = df.drop(columns=['guild_id'])#.rename(columns={'guild_name': 'guild_name'})
-            args.exp_var = 'guild_name'
-        return df
+        return df_grouped
 
 
-    def visualize_graph(self, df, args):
-        if len(df) == 0:
-            raise Exception("The dataframe is empty")
-
+    def prettify_graph(self, df, args):
         if args.log:
             plt.yscale('log')
 
+        #Final df edits
         x_axis = args.exp_var if args.exp_var else 'index'
         df[x_axis] = df[x_axis].astype(str)
         df.sort_values(args.ind_var, ascending=True, inplace=True)
 
+        #Plotting
         plt.bar(df[x_axis], df[args.ind_var])
+
+        #Labeling
         title = f"{args.ind_var} by {x_axis}" + (f" over the past {args.period}" if args.period else "") + (f" (log scale)" if args.log else "")
         plt.title(title)
         plt.xlabel(x_axis)
         plt.ylabel(args.ind_var)
         plt.tight_layout()
+
+
+    def make_graph(self, df, args):
+        if len(df) == 0:
+            raise Exception("The dataframe is empty")
+
+        self.prettify_graph(df, args)
 
         if self.show_fig:
             plt.show()
@@ -146,7 +164,6 @@ class Reporter():
         path = f"./state/graphs/{args.ind_var}_{args.exp_var or 'index'}_{args.period or 'all'}.png"
         plt.savefig(path)
         return path
-
 
         # buffer = io.BytesIO()
         # plt.savefig(buffer, format='png') #Save the file to a io.BytesIO object instead of a path
@@ -162,7 +179,7 @@ class Reporter():
         sys.argv = args
 
         parser = ArgumentParser(description="Visualize data from selected metrics.")
-        parser.add_argument("-df", "--dataframe", required=True, choices=self.csvHandler.df_options, help="Choose the dataframe.")
+        parser.add_argument("-df", "--dataframe", required=True, help="Choose the dataframe.")
         parser.add_argument("-iv", "--ind_var", required=True, help="Independent variable to analyze.")
         parser.add_argument("-p", "--period", choices=self.time_periods.keys(), help="Time period to filter data.")
         parser.add_argument("-ev", "--exp_var", help="Explanatory variable to pivot by.")
@@ -170,6 +187,7 @@ class Reporter():
         parser.add_argument("-ln", "--log", action="store_true", help="Enable logarithmic scale for the y-axis.")
         parser.add_argument("-avg", "--average", action="store_true", help="Averages, not sums, the ind_var")
         return parser.parse_args()
+
 
     def arg_to_string(self, args):
         components = [args.dataframe, args.ind_var]
@@ -184,52 +202,29 @@ class Reporter():
 
         df = self.select_dataframe(args.dataframe)
 
-        df = self.preprocessing(df, args)
+        df_limited = self.prepare_df(df, args)
 
-        if args.ind_var not in df.columns:
-            raise ArgumentError(None, f"Invalid independent variable: {args.ind_var}")
+        graph = self.make_graph(df_limited, args)
 
-        if args.exp_var and args.exp_var not in df.columns:
-            raise ArgumentError(None, f"Invalid explanatory variable: {args.exp_var}")
+        graph_string = self.arg_to_string(args)
 
-        if args.period and args.period not in self.time_periods:
-            raise ArgumentError(None, f"Invalid time period: {args.period}")
-
-        df_limited = self.edit_df(df, args)
-
-        return self.arg_to_string(args), self.visualize_graph(df_limited, args)
+        return graph_string, graph
 
 
 if __name__ == '__main__':
-    """
-    Dr. Bean said an initial approach to the metrics would be to make flags that would specify the data desired.
-    So, for example:
-    >>> !report -df usage -ind_var cost -p week -exp_var server
-    *Spit out an image that shows the last week's cost per server*
-    
-    Preferably there are some pre-baked reports that should just be accessible with certain key words. IE:
-    >>> !report report 1
-    *Spit out an image that shows the last week's cost per server*
-    
-    Additionally, there should be some sort of help menu?
-    >>> !report help
-    *show the available list of flags and the order they'd need to be processed*
-    """
     from pathlib import Path
 
-    # Convert a string into a Path object
     metricsHandler = MetricsHandler(Path('./state/metrics'))
     reporter = Reporter(metricsHandler, True)
     #Initialize the metricsHandler, should be what's returning the table that I then convert in the reporter to a pd df
     sys_string = '!report u2'
-
 
     try:
         if sys_string not in reporter.image_cache:
             arg_string, image_path = reporter.get_report(sys_string)
             reporter.image_cache[sys_string] = image_path
         else:
-            image = reporter.image_cache[sys_string]
+            image_path = reporter.image_cache[sys_string]
 
     except Exception as e:
         print(f"An error occurred: {e}")
