d1929a4674f1737d54eecd4b6d02461ac586c182
Wiley Welch <wileyjwelch1@gmail.com>
2025-01-25 12:40:28 -0700

    Confirmed the logging works so I took it out and I'm going to submit a pull request

--- rubber_duck/discord_bot.py
+++ rubber_duck/discord_bot.py
@@ -360,6 +360,4 @@ if __name__ == '__main__':
     if config is None:
         # If fetching from S3 failed, load from local file
         config = load_local_config(args.config)
-    else:
-        logging.info(f"Config file is working here is the config: {json.dumps(config, indent=2)}")
     main(config)
