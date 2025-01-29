8a9af6df7d97ac13ba5a4f131f6429819f0a0414
Aubry <ahiatt19@byu.edu>
2025-01-29 10:51:42 -0700

    1: Started the initial transition from SQLite to SQLAlchemy. 2: Now implementing the connection to the database Nic implemented.

--- rubber_duck/SQLmetrics.py
+++ rubber_duck/SQLmetrics.py
@@ -2,104 +2,105 @@ import sqlite3
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from quest import step
+from sqlalchemy import Column, Integer, String, DATETIME
+from sqlalchemy.orm import Mapped, declarative_base, mapped_column, sessionmaker
 
-def setUpConnection():
-    # Connection Object
-    connection = sqlite3.connect('../state/database.db')
-    # Cursor object
-    cursor = connection.cursor()
-    return connection, cursor
-    # Needs to figure out
-    # connection.close()
+from connection import DatabaseConnection
 
+Base = declarative_base()
 
 def get_timestamp():
     return datetime.now(ZoneInfo('US/Mountain')).isoformat()
 
+class MessagesModel(Base):
+    __tablename__ = 'messages'
+
+    timestamp = Column(String)
+    guild_id = Column(Integer)
+    thread_id = Column(Integer)
+    user_id = Column(Integer)
+    role = Column(String)
+    message = Column(String)
+
+
+class UsageModel(Base):
+    __tablename__ = 'usage'
+
+    timestamp = Column(String)
+    guild_id = Column(Integer)
+    thread_id = Column(Integer)
+    user_id = Column(Integer)
+    engine = Column(String)
+    input_tokens = Column(String)
+    output_tokens = Column(String)
+
+
+class FeedbackModel(Base):
+    __tablename__ = 'usage'
+
+    timestamp = Column(String)
+    guild_id = Column(Integer)
+    thread_id = Column(Integer)
+    user_id = Column(Integer)
+    reviewer_role_id = Column(Integer)
+    feedback_score = Column(Integer)
+
+
 
 class SQLMetricsHandler:
     def __init__(self):
-        self.connection, self.cursor = setUpConnection()
-        # SQL Create Table Strings
-        createMessagesTable = """ 
-            CREATE TABLE IF NOT EXISTS messages (
-                timestamp DATETIME,
-                guild_id INT,
-                thread_id INT,
-                user_id INT,
-                role TEXT,
-                message TEXT
-                ); """
-        createUsageTable = """ 
-            CREATE TABLE IF NOT EXISTS usage (
-                timestamp DATETIME,
-                guild_id INT,
-                thread_id INT,
-                user_id INT,
-                engine TEXT,
-                input_tokens TEXT,
-                output_tokens TEXT
-                ); """
-        createFeedbackTable = """ 
-            CREATE TABLE IF NOT EXISTS feedback (
-                timestamp DATETIME,
-                guild_id INT,
-                thread_id INT,
-                user_id INT,
-                reviewer_role_id INT,
-                feedback_score INT
-                ); """
-        # Execute the SQL queries
-        self.cursor.execute(createMessagesTable)
-        self.connection.commit()
-        self.cursor.execute(createUsageTable)
-        self.connection.commit()
-        self.cursor.execute(createFeedbackTable)
-        self.connection.commit()
+        self.db = DatabaseConnection(Base)
+        self.session = self.db.get_session()
 
     @step
     async def record_message(self, guild_id: int, thread_id: int, user_id: int, role: str, message: str):
         try:
-            insertMessage = """ 
-                INSERT INTO messages (timestamp, guild_id, thread_id, user_id, role, message)
-                    VALUES($1, $2, $3, $4, $5, $6); """
-            self.cursor.execute(insertMessage, (get_timestamp(), guild_id, thread_id, user_id, role, message))
-            self.connection.commit()
+            newMessageRow = MessagesModel(timestamp=get_timestamp(), guild_id=guild_id, thread_id=thread_id, user_id=user_id,
+                                       role=role, message=message)
+            self.session.add(newMessageRow)
+            self.session.commit()
         except sqlite3.Error as e:
             print(f"An error occured: {e}")
 
     @step
     async def record_usage(self, guild_id, thread_id, user_id, engine, input_tokens, output_tokens):
         try:
-            insertUsage = """ 
-                INSERT INTO usage (timestamp, guild_id, thread_id, user_id, engine, input_tokens, output_tokens)
-                    VALUES($1, $2, $3, $4, $5, $6, $7); """
-            self.cursor.execute(insertUsage, (get_timestamp(), guild_id, thread_id, user_id, engine, input_tokens, output_tokens))
-            self.connection.commit()
+            newUsageRow = UsageModel(timestamp=get_timestamp(), guild_id=guild_id, thread_id=thread_id, user_id=user_id,
+                                  engine=engine, input_tokens=input_tokens, output_tokens=output_tokens)
+            self.session.add(newUsageRow)
+            self.session.commit()
         except sqlite3.Error as e:
             print(f"An error occured: {e}")
 
     @step
     async def record_feedback(self, guild_id: int, thread_id: int, user_id: int, feedback_score: int, reviewer_id: int):
         try:
-            insertFeedback = """ 
-                INSERT INTO feedback (timestamp, guild_id, thread_id, user_id, feedback_score, reviewer_id)
-                    VALUES($1, $2, $3, $4, $5, $6); """
-            self.cursor.execute(insertFeedback, (get_timestamp(), guild_id, thread_id, user_id, feedback_score, reviewer_id))
-            self.connection.commit()
+            newFeedbackRow = FeedbackModel(timestamp=get_timestamp(), guild_id=guild_id, thread_id=thread_id, user_id=user_id,
+                                           reviewer_role_id=reviewer_id, feedback_score=feedback_score)
+            self.session.add(newFeedbackRow)
+            self.session.commit()
         except sqlite3.Error as e:
             print(f"An error occured: {e}")
 
     def get_message(self):
-        messagesTable = """ SELECT * FROM messages """
-        return self.cursor.execute(messagesTable)
+        try:
+            messagesTable = """ SELECT * FROM messages """
+            return self.cursor.execute(messagesTable)
+        except sqlite3.Error as e:
+            print(f"An error occured: {e}")
 
 
     def get_usage(self):
-        usageTable = """ SELECT * FROM usage """
-        return self.cursor.execute(usageTable)
+        try:
+            usageTable = """ SELECT * FROM usage """
+            return self.cursor.execute(usageTable)
+        except sqlite3.Error as e:
+            print(f"An error occured: {e}")
 
 
     def get_feedback(self):
-        feedbackTable = """ SELECT * FROM feedback """
-        return self.cursor.execute(feedbackTable)
+        try:
+            feedbackTable = """ SELECT * FROM feedback """
+            return self.cursor.execute(feedbackTable)
+        except sqlite3.Error as e:
+            print(f"An error occured: {e}")
