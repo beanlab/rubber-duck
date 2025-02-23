564b9473605f2b0470b1c5a70b973c67c44a8285
niclew17 <niclewis17@gmail.com>
2025-02-22 19:24:57 -0700

    Added general sql functionality

--- rubber_duck/sql_metrics.py
+++ rubber_duck/sql_metrics.py
@@ -2,7 +2,7 @@ import sqlite3
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, create_engine, BigInteger
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
 
 MetricsBase = declarative_base()
@@ -16,38 +16,38 @@ class MessagesModel(MetricsBase):
     __tablename__ = 'messages'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    timestamp = Column(String)
-    guild_id = Column(Integer)
-    thread_id = Column(Integer)
-    user_id = Column(Integer)
-    role = Column(String)
-    message = Column(String)
+    timestamp = Column(String(255))
+    guild_id = Column(BigInteger)
+    thread_id = Column(BigInteger)
+    user_id = Column(BigInteger)
+    role = Column(String(255))
+    message = Column(String(4096))
 
 
 class UsageModel(MetricsBase):
     __tablename__ = 'usage'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    timestamp = Column(String)
-    guild_id = Column(Integer)
-    thread_id = Column(Integer)
-    user_id = Column(Integer)
-    engine = Column(String)
-    input_tokens = Column(String)
-    output_tokens = Column(String)
+    timestamp = Column(String(255))
+    guild_id = Column(BigInteger)
+    thread_id = Column(BigInteger)
+    user_id = Column(BigInteger)
+    engine = Column(String(255))
+    input_tokens = Column(String(255))
+    output_tokens = Column(String(255))
 
 
 class FeedbackModel(MetricsBase):
     __tablename__ = 'feedback'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    timestamp = Column(String)
-    workflow_type = Column(String)
-    guild_id = Column(Integer)
-    thread_id = Column(Integer)
-    user_id = Column(Integer)
-    reviewer_role_id = Column(Integer)
-    feedback_score = Column(Integer)
+    timestamp = Column(String(255))
+    workflow_type = Column(String(255))
+    guild_id = Column(BigInteger)
+    thread_id = Column(BigInteger)
+    user_id = Column(BigInteger)
+    reviewer_role_id = Column(BigInteger)
+    feedback_score = Column(BigInteger)
 
 
 
