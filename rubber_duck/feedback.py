951763c5c5039df3a0faef0c0374a3ce1db771f4
Aubry <ahiatt19@byu.edu>
2025-02-24 11:46:47 -0700

    Got rid of feedback changes 2/2.

--- rubber_duck/feedback.py
+++ rubber_duck/feedback.py
@@ -90,8 +90,11 @@ class GetTAFeedback:
                 feedback_score = 'nan'
                 reviewer_id = 'nan'
 
+            #Record score
+
             await self._record_feedback(workflow_type, guild_id, thread_id, user_id, reviewer_id, feedback_score)
 
+    #Done
 
     @staticmethod
     async def _get_reviewer_feedback(user_id, feedback_queue, allow_self_feedback, feedback_timeout):
