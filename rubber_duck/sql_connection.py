e4e42488818567fdc3cc4d92ca53032f99be63b2
Wiley Welch <wileyjwelch1@gmail.com>
2025-03-03 11:46:51 -0700

    test 1

--- rubber_duck/sql_connection.py
+++ rubber_duck/sql_connection.py
@@ -1,6 +1,7 @@
 import os
 from typing import TypedDict
 
+from distlib.util import resolve
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
@@ -12,8 +13,6 @@ def _create_sqlite_session(db_name: str) -> Session:
 
 
 def _create_sql_session(db_type: str, username: str, password: str, host: str, port: str, database: str) -> Session:
-    username = os.getenv(username)
-    password = os.getenv(password)
     db_url = f"{db_type}://{username}:{password}@{host}:{port}/{database}"
     engine = create_engine(db_url)
     return sessionmaker(bind=engine)()
@@ -28,7 +27,15 @@ class SqlConfig(TypedDict):
     database: str
 
 
+def resolve_env_vars(config):
+    return {
+        key: os.getenv(value[len('env:'):]) if value.startswith('env:') else value
+        for key, value in config.items()
+    }
+
+
 def create_sql_session(config: SqlConfig) -> Session:
+    config = resolve_env_vars(config)
     if config['db_type'] == 'sqlite':
         return _create_sqlite_session(config['database'])
     else:
