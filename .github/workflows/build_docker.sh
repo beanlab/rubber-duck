436c3507f49c6ad858a7c97ec6a9510b583aa7eb
Wiley Welch <wileyjwelch1@gmail.com>
2025-03-04 18:18:57 -0700

    Updated the build folder for the script part 2

--- .github/workflows/build_docker.sh
+++ .github/workflows/build_docker.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-
 # Fetch the Git commit hash or branch name to create a unique tag
 IMAGE_NAME="rubber-duck"
-IMAGE_TAG=$(git rev-parse --abbrev-ref HEAD) #use branch name if you prefer: IMAGE_TAG=$(git rev-parse --abbrev-ref HEAD)
-
+IMAGE_TAG=$(git rev-parse --abbrev-ref HEAD)  # using branch name as tag
 
 ECR_REPO="844825014198.dkr.ecr.us-west-2.amazonaws.com/beanlab/rubber-duck"
 
 set -e
 
-# Build the Docker image
-docker build -t ${IMAGE_NAME}:${IMAGE_TAG} -f - ../../ <<EOF
+# Change to the repository root
+cd ../../
 
+# Build the Docker image using the repository root ('.') as the build context
+docker build -t ${IMAGE_NAME}:${IMAGE_TAG} -f - . <<EOF
 FROM python:3.11.9
 LABEL authors="Wiley Welch, Bryce Martin, Gordon Bean"
 COPY rubber_duck /rubber_duck
@@ -27,9 +27,10 @@ WORKDIR /
 CMD ["python", "/rubber_duck/discord_bot.py", "--config", "/config.json", "--log-console"]
 EOF
 
-# Tag the image for ECR with the unique tag (commit hash or branch name)
+# Tag the image for ECR with the unique tag (branch name)
 docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${ECR_REPO}:${IMAGE_TAG}
 
-# Optionally, also tag as "latest" for easier reference
+# Also tag as "latest" for easier reference (and optionally as "latest-latest" if needed)
 docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${ECR_REPO}:latest
 docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${ECR_REPO}:latest-latest
+
