ff8725661550841e61c95aaaf919c1e911bde3da
Aubry <ahiatt19@byu.edu>
2025-03-05 14:34:59 -0700

    Refactored the command and bot+commands file.

--- rubber_duck/bot_commands.py
+++ rubber_duck/bot_commands.py
@@ -4,23 +4,16 @@ from command import Command
 
 
 class BotCommands:
-    def __init__(self, send_message, metrics_handler, reporter):
-        self._send_message = step(send_message)
-        self._metrics_handler = metrics_handler
-        self._reporter = reporter
+    def __init__(self, commands: {}):
+        self.commands = commands
 
     async def __call__(self, message: Message):
         return await self.handle_command(message)
 
     async def handle_command(self, message: Message):
+        # Extract the command from the content
         content = message['content']
-        channel_id = message['channel_id']
-
-        # Extract the command
         command_name = content.split()[0]
         # Get the corresponding class
-        command_class = Command.get_command(command_name)
-
-        # Instantiate and execute the command
-        command_instance = command_class(self._send_message, self._metrics_handler, self._reporter)
-        await command_instance.execute(content, channel_id)
+        command_class = self.commands[command_name]
+        await command_class.execute(message)
--- rubber_duck/command.py
+++ rubber_duck/command.py
@@ -4,11 +4,123 @@ import subprocess
 
 from pathlib import Path
 from quest import step
+from protocols import Message
 
 
 class Command():
-    async def execute(self, content: str, channel_id: int):
+    help_msg = ""
+
+    async def execute(self, message: Message):
         pass
+
+class MessagesMetricsCommand():
+    help_msg = "!messages - get a zip of the messages data\n"
+
+    def __init__(self, send_message, metrics_handler):
+        self.send_message = send_message
+        self.metrics_handler = metrics_handler
+
+    @step
+    async def execute(self, message: Message):
+        channel_id = message['channel_id']
+        messages_zip = reporter.zip_data_file(self.metrics_handler.get_messages())
+        discord_messages_file = discord.File(messages_zip, filename="messages.zip")
+        await self.send_message(channel_id, "", file=discord_messages_file)
+
+
+class UsageMetricsCommand():
+    help_msg = "!usage - get a zip of the usage data\n"
+
+    def __init__(self, send_message, metrics_handler):
+        self.send_message = send_message
+        self.metrics_handler = metrics_handler
+
+    @step
+    async def execute(self, message: Message):
+        channel_id = message['channel_id']
+        usage_zip = reporter.zip_data_file(self.metrics_handler.get_usage())
+        discord_usage_file = discord.File(usage_zip, filename="usage.zip")
+        await self.send_message(channel_id, "", file=discord_usage_file)
+
+
+class FeedbackMetricsCommand():
+    help_msg = "!feedback - get a zip of the feedback data\n"
+
+    def __init__(self, send_message, metrics_handler):
+        self.send_message = send_message
+        self.metrics_handler = metrics_handler
+
+    @step
+    async def execute(self, message: Message):
+        channel_id = message['channel_id']
+        feedback_zip = reporter.zip_data_file(self.metrics_handler.get_feedback())
+        discord_feedback_file = discord.File(feedback_zip, filename="feedback.zip")
+        await self.send_message(channel_id, "", file=discord_feedback_file)
+
+
+
+class MetricsCommand():
+    help_msg = "!metrics - get the zips of the data tables\n"
+
+    def __init__(self, messages_metrics: MessagesMetricsCommand, usage_metrics: UsageMetricsCommand, feedback_metrics: FeedbackMetricsCommand):
+        self.messages_metrics = messages_metrics
+        self.usage_metrics = usage_metrics
+        self.feedback_metrics = feedback_metrics
+
+    @step
+    async def execute(self, message: Message):
+        await self.messages_metrics.execute(message)
+        await self.usage_metrics.execute(message)
+        await self.feedback_metrics.execute(message)
+
+
+class StatusCommand():
+    help_msg = "!status - print a status message\n"
+
+    def __init__(self, send_message):
+        self.send_message = send_message
+
+    async def execute(self, message: Message):
+        channel_id = message['channel_id']
+        await self.send_message(channel_id, 'I am alive. 🦆')
+
+
+# class HelpCommand():
+#     async def execute(self, content: str, channel_id: int):
+#         commands_list = "\n".join(Command._registry.keys())  # Get all registered commands
+#         help_message = f"```\n{commands_list}\n```"
+#         await self._send_message(channel_id, help_message)
+        # "!status - print a status message\n"
+        # "!help - print this message\n"
+        # "!log - get the log file\n"
+        # "!metrics - get the zips of the data tables\n"
+        # "!state - get a zip of the state folder\n"
+        # "!restart - restart the bot\n"
+        # "!clean-restart - wipe the state and restart the bot\n"
+
+
+class ReportCommand():
+    help_msg = ""
+
+    def __init__(self, send_message, reporter):
+        self.send_message = send_message
+        self.reporter = reporter
+
+    @step
+    async def execute(self, message: Message):
+        content = message['content']
+        channel_id = message['channel_id']
+        img_name, img = self.reporter.get_report(content)
+        if img is None:
+            await self.send_message(channel_id, img_name)
+
+        elif isinstance(img, list):
+            imgs = [discord.File(fp=image, filename=image_name) for image, image_name in zip(img, img_name)]
+            await self.send_message(channel_id, img_name, files=imgs)
+
+        else:
+            await self.send_message(channel_id, img_name, file=discord.File(fp=img, filename=img_name))
+
 
 
 class BashExecuteCommand():
@@ -40,96 +152,43 @@ class BashExecuteCommand():
             await self.send_message(channel_id, f'```{output}```')
 
 
-class LogCommand():
-    def __init__(self, send_message):
-        self.send_message = send_message
-
-    @step
-    async def execute(self, content: str, channel_id: int):
-        await self.send_message(channel_id, 'The log command has been temporarily disabled.')
-        #await self._execute_command(channel_id, f'zip -q -r log.zip {self._log_file_path}')
-        #await self._send_message(channel_id, 'log zip', file='log.zip')
-
-
-class MetricsCommand():
-    def __init__(self, send_message, metrics_handler):
-        self.send_message = send_message
-        self.metrics_handler = metrics_handler
-
-    @step
-    async def execute(self, content: str, channel_id: int):
-        messages_zip = reporter.zip_data_file(self.metrics_handler.get_messages())
-        usage_zip = reporter.zip_data_file(self.metrics_handler.get_usage())
-        feedback_zip = reporter.zip_data_file(self.metrics_handler.get_feedback())
-
-        discord_messages_file = discord.File(messages_zip, filename="messages.zip")
-        discord_usage_file = discord.File(usage_zip, filename="usage.zip")
-        discord_feedback_file = discord.File(feedback_zip, filename="feedback.zip")
-
-        await self.send_message(channel_id, "", file=discord_messages_file)
-        await self.send_message(channel_id, "", file=discord_usage_file)
-        await self.send_message(channel_id, "", file=discord_feedback_file)
-
-
-class StatusCommand():
-    def __init__(self, send_message):
-        self.send_message = send_message
-
-    async def execute(self, content: str, channel_id: int):
-        await self.send_message(channel_id, 'I am alive. 🦆')
-
-
-# class HelpCommand():
-#     async def execute(self, content: str, channel_id: int):
-#         commands_list = "\n".join(Command._registry.keys())  # Get all registered commands
-#         help_message = f"```\n{commands_list}\n```"
-#         await self._send_message(channel_id, help_message)
-        # "!status - print a status message\n"
-        # "!help - print this message\n"
-        # "!log - get the log file\n"
-        # "!metrics - get the zips of the data tables\n"
-        # "!state - get a zip of the state folder\n"
-        # "!restart - restart the bot\n"
-        # "!clean-restart - wipe the state and restart the bot\n"
-
-
-class ReportCommand():
-    def __init__(self, send_message, reporter):
-        self.send_message = send_message
-        self.reporter = reporter
-
-    @step
-    async def execute(self, content: str, channel_id: int):
-        img_name, img = self.reporter.get_report(content)
-        if img is None:
-            await self.send_message(channel_id, img_name)
-
-        elif isinstance(img, list):
-            imgs = [discord.File(fp=image, filename=image_name) for image, image_name in zip(img, img_name)]
-            await self.send_message(channel_id, img_name, files=imgs)
-
-        else:
-            await self.send_message(channel_id, img_name, file=discord.File(fp=img, filename=img_name))
-
 
 class StateCommand():
+    help_msg = ""
+
     def __init__(self, send_message, bash_execute_command: BashExecuteCommand):
         self.send_message = send_message
         self.bash_execute_command = bash_execute_command
 
     @step
-    async def execute(self, content: str, channel_id: int):
+    async def execute(self, message: Message):
+        channel_id = message['channel_id']
         await self.send_message(channel_id, "Getting state zip")
         await self.bash_execute_command.execute_command(channel_id, 'zip -q -r state.zip state')
         await self.send_message(channel_id, 'state zip', file='state.zip')
 
 
+class LogCommand():
+    help_msg = "!log - get the log file\n"
+
+    def __init__(self, send_message, bash_execute_command: BashExecuteCommand):
+        self.send_message = send_message
+
+    @step
+    async def execute(self, message: Message):
+        channel_id = message['channel_id']
+        await self.send_message(channel_id, 'The log command has been temporarily disabled.')
+        #await self._execute_command(channel_id, f'zip -q -r log.zip {self._log_file_path}')
+        #await self._send_message(channel_id, 'log zip', file='log.zip')
+
+
 class UnknownCommand():
+    help_msg = ""
+
     def __init__(self, send_message, ):
         self.send_message = send_message
 
-    async def execute(self, content: str, channel_id: int):
+    async def execute(self, message: Message):
+        channel_id = message['channel_id']
         await self.send_message(channel_id, 'Unknown command. Try !help')
-
-
 
