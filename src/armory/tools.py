3df65ddf59cd30b8f45c8e8a6814362b79a2a38f
niclew17 <niclewis17@gmail.com>
2025-05-19 08:12:13 -0600

    added a load tools function

--- src/armory/tools.py
+++ src/armory/tools.py
@@ -1,6 +1,3 @@
-import importlib
-import pkgutil
-
 from agents import function_tool, FunctionTool
 
 _tools: dict[str, FunctionTool] = {}
@@ -11,22 +8,19 @@ def register_tool(func) -> FunctionTool:
     _tools[func.__name__] = tool
     return tool
 
+def load_tools():
+
+    import importlib
+    import pkgutil
+
+    package = importlib.import_module(__package__)
+    for _, module_name, _ in pkgutil.iter_modules(package.__path__):
+        importlib.import_module(f"{package.__name__}.{module_name}")
 
 def get_tool(tool_name: str):
     if tool_name in _tools:
         return _tools[tool_name]
 
-    # The way we're accessing a tool before it is actually used means that we need to import them so that the @register_tool will run
-    # and add that tool to tool_registry. This is here to dynamically import all tools so that they are available. I don't know if there
-    # is a better way to do this
-
-    import src.armory
-    package = src.armory
-
-    for _, module_name, _ in pkgutil.iter_modules(package.__path__):
-        importlib.import_module(f"{package.__name__}.{module_name}")
-
-        if tool_name in _tools:
-            return _tools[tool_name]
-
     raise KeyError(f"Tool '{tool_name}' not found in any armory module.")
+
+load_tools()
