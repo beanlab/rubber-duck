2b57e1f335d454a587ed8a8d4f7fb03a4ee73369
rebek <math919191@gmail.com>
2025-11-29 22:41:47 -0700

    refactor Rules, updated rubrics

--- src/workflows/assignment_feedback_workflow.py
+++ src/workflows/assignment_feedback_workflow.py
@@ -7,9 +7,10 @@ import yaml
 from quest import step, task
 
 from .parsing_utils import is_filled_in_report, unflatten_dictionary, dict_to_md, \
-    find_project_name_in_report_headers, get_expected_md_format
+    find_project_name_in_report_headers
 from ..gen_ai.gen_ai import Agent, AIClient
 from ..utils.config_types import DuckContext, AssignmentFeedbackSettings, Gradable, RubricItemResponse
+from ..utils.logger import duck_logger
 from ..utils.message_utils import wait_for_message
 from ..utils.protocols import ConversationComplete
 
@@ -61,10 +62,12 @@ class AssignmentFeedbackWorkflow:
                 await self._send_message(context.thread_id, assignment_message)
 
             rubric_contents = await self._load_rubric(project_name)
+            rules = await self._load_rules(project_name)
 
             graded_results = await self._grade_assignment(context,
                                                           report_contents,
-                                                          rubric_contents)
+                                                          rubric_contents,
+                                                          rules)
 
             await self._send_message(context.thread_id, graded_results)
 
@@ -75,18 +78,39 @@ class AssignmentFeedbackWorkflow:
     @step
     async def _load_rubric(self, project_name):
         rubric_files = self._assignments[project_name].get("rubric_path")
-
         if isinstance(rubric_files, str):
             rubric_files = [rubric_files]
 
         contents = '\n'.join(Path(file).read_text() for file in rubric_files)
+        all_loaded_contents = yaml.safe_load(contents)
+        rubric_contents = all_loaded_contents.get(project_name)
 
-        return yaml.safe_load(contents)
+        if not rubric_contents:
+            duck_logger.warn(f"No rubric for {project_name}")
 
+        return {project_name: rubric_contents}
 
     @step
-    async def _grade_assignment(self, context, report_contents, rubric_contents) -> str:
-        flattened_report_and_rubric_items = self._flatten_report_and_rubric_items(report_contents, rubric_contents)
+    async def _load_rules(self, project_name):
+        rubric_files = self._assignments[project_name].get("rubric_path")
+        if isinstance(rubric_files, str):
+            rubric_files = [rubric_files]
+
+        rules = {}
+        for file in rubric_files:
+            loaded_text = yaml.safe_load(Path(file).read_text())
+
+            for key, value in loaded_text.items():
+                if key == 'Rules':
+                    rules |= value # merge the rule dictionaries
+
+        return rules
+
+    @step
+    async def _grade_assignment(self, context, report_contents, rubric_contents, rules) -> str:
+        flattened_report_and_rubric_items = self._flatten_report_and_rubric_items(report_contents,
+                                                                                  rubric_contents,
+                                                                                  rules)
 
         flattened_graded_items = [
             await self._grade_single_item(context, piece_name, report_section, rubric_item, rules)
@@ -109,7 +133,8 @@ class AssignmentFeedbackWorkflow:
         emoji = ':white_check_mark:' if response['satisfactory'] else ':x:'
 
         # Reformat rubric item: fruit [rule_name] -> fruit
-        formatted_rubric_item = response["rubric_item"].split('[',1)[0].strip()
+        formatted_rubric_item = response["rubric_item"].split('[', 1)[0].strip()
+
         return f'{emoji} **{formatted_rubric_item}** - {response["justification"]}'
 
     @step
@@ -160,21 +185,29 @@ class AssignmentFeedbackWorkflow:
 
         raise ConversationComplete("No markdown files were uploaded")
 
-    def _get_rules(self, rubric_item, rubric_contents):
-        if match := re.search('\[(.*?)\]', rubric_item):
-            rules = match.group(1)
-            rule_names = rules.split(',')
+    def _get_rules(self, rubric_item, rules):
+        # check to see if any rules are present (i.e. Talks about fruit [Rule 1, Rule 2])
+        match = re.search('\[(.*?)\]', rubric_item)
 
-            rules = rubric_contents.get('_Rules')
+        if not match:
+            return None
 
-            rule_contents = [rules.get(rule_name) for rule_name in rule_names]
-            rule_contents = '----\n\n'.join(rule_contents)
-            return rule_contents
-        return None
+        rules_for_rubric_item = match.group(1)
+        rule_names = [rule.strip() for rule in rules_for_rubric_item.split(',')]
 
+        rule_contents = {}
+        for rule_name in rule_names:
+            rule_content = rules.get(rule_name, '')
+            rule_contents[rule_name] = rule_content
+
+            if not rule_content:
+                duck_logger.warn(f'Rule name "{rule_name}" not found')
+                continue
+
+        return rule_contents
 
     @step
-    async def _grade_single_item(self, context, piece_name, report_section, rubric_item, rules='') -> tuple[
+    async def _grade_single_item(self, context, piece_name, report_section, rubric_item, rules) -> tuple[
         list[SECTION_NAME], RubricItemResponse]:
 
         if not is_filled_in_report(report_section):
@@ -200,11 +233,12 @@ class AssignmentFeedbackWorkflow:
         result: RubricItemResponse = json.loads(raw_response)
         return piece_name, result
 
-    def _flatten_report_and_rubric_items(self, report_contents, rubric_contents) -> list[
+    def _flatten_report_and_rubric_items(self, report_contents, rubric_contents, rules) -> list[
         tuple[list[SECTION_NAME], RUBRIC_ITEM, REPORT_SECTION, RULES]]:
         def helper_func(name, rubric, report_section):
             for section_name, rubric_content in rubric.items():
-                if section_name[0] == '_':  # ignore any headers that start with '_'
+                # ignore any headers that start with '_' or "Rules"
+                if section_name[0] == '_' or re.match('^Rules.*', section_name):
                     continue
 
                 name.append(section_name)
@@ -218,10 +252,9 @@ class AssignmentFeedbackWorkflow:
 
                 elif isinstance(rubric_content, list):
                     for section_item in rubric_content:
+                        rubric_item_rules = self._get_rules(section_item, rules)
 
-                        rules = self._get_rules(section_item, rubric_contents)
-
-                        yield name[::], section_item, report_section[section_name], rules
+                        yield name[::], section_item, report_section[section_name], rubric_item_rules
                 name.pop(-1)
 
         flattened = list(helper_func([], rubric_contents, report_contents))
