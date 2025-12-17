9cae2472e29c4d5cb14324cca660b216a94acbc0
Tyler <tyguyreeses@gmail.com>
2025-12-16 22:45:40 -0700

    remove datastore in python tools

--- src/main.py
+++ src/main.py
@@ -265,7 +265,7 @@ def build_armory(config: Config, send_message, data_store: DataStore, containers
     for tool_config in config_tools:
         if tool_config['type'] == 'container_exec':
             container_name = tool_config['container']
-            python_tools = PythonTools(containers[container_name], data_store, send_message)
+            python_tools = PythonTools(containers[container_name], send_message)
             amended_description = (
                     tool_config.get('description', python_tools.run_code.__doc__)
                     + '\n'
