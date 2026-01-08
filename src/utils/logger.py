a3f22ed13fca69a4aed065a2fdca994e3d3e3704
Tyler <tyguyreeses@gmail.com>
2026-01-07 22:19:03 -0700

    remove typo

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -38,7 +38,7 @@ def add_console_handler():
 
     duck_logger.addHandler(console_handler)
     quest_logger.addHandler(console_handler)
-ç
+
 
 def add_file_handler(file_path: str):
     """Add a file handler to the duck logger."""
@@ -87,7 +87,6 @@ ERROR_RE = re.compile(
     """,
     re.VERBOSE | re.DOTALL
 )
-
 
 
 def format_error_message(raw: str) -> str:
