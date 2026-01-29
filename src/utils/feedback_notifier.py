3c52fb991109e32300127c2fb16a595c57aae2cd
Tyler <tyguyreeses@gmail.com>
2026-01-28 22:37:37 -0700

    update build_feedback_queues to use dict-based config

--- src/utils/feedback_notifier.py
+++ src/utils/feedback_notifier.py
@@ -79,15 +79,15 @@ class FeedbackNotifier:
         feedback_mapping = {}
 
         for server_config in self._server_configs:
-            for channel in server_config['channels']:
-                if channel['channel_name'] == 'ta-review':
-                    ta_channel_id = channel['channel_id']
+            for channel_name, channel_cfg in server_config['channels'].items():
+                if channel_name == 'ta-review':
+                    ta_channel_id = channel_cfg['channel_id']
                     target_channels = []
 
                     # Find all ducks of type conversation_review in this channel
-                    for duck in channel['ducks']:
-                        if duck['duck_type'] == 'conversation_review':
-                            target_channels.extend(duck['settings']['target_channel_ids'])
+                    duck = channel_cfg['duck']
+                    if duck['duck_type'] == 'conversation_review':
+                        target_channels.extend(duck['settings']['target_channel_ids'])
 
                     if target_channels:
                         feedback_mapping[ta_channel_id] = target_channels
